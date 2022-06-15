2185caf7a78ef40f129ac7eed6798f6713cae728
Aljaz Simonic <aljaz.simonic@purple.telstra.com>
2022-06-15 21:44:52 +1000

    Update exercise.

--- solution.ps1
+++ solution.ps1
@@ -1,8 +1,10 @@
+Set-Location $PSScriptRoot
+
 # create Dockerfile in VS
 
-# move Dockerfile to parent directory
-Set-Location -Path ".Source"
-Move-Item -Path "./WeatherApi/Dockerfile" -Destination "./Dockerfile"
+# copy Dockerfile to parent directory
+Set-Location -Path "./Source"
+Copy-Item -Path "./WeatherApi/Dockerfile" -Destination "./Dockerfile"
 
 # list images
 docker image ls
@@ -14,19 +16,19 @@ docker image build --tag weatherapi:latest .
 docker image ls
 
 # list containers
-docker container ls
+docker container ls --all
 
 # create "brisbane" container
 docker container create --publish 9991:80 --name weatherapibrisbane weatherapi
 
 # list containers
-docker container ls
+docker container ls --all
 
 # start "brisbane" container
 docker container start weatherapibrisbane
 
 # list containers
-docker container ls
+docker container ls --all
 
 # verify "brisbane" container is accessible from browser
 Start-Process "http://localhost:9991/weather"
@@ -35,22 +37,31 @@ Start-Process "http://localhost:9991/weather"
 docker container create --publish 9992:80 --env "Weather:City=Melbourne" --name weatherapimelbourne weatherapi
 
 # list containers
-docker container ls
+docker container ls --all
 
 # start "melbourne" container
 docker container start weatherapimelbourne
 
 # list containers
-docker container ls
+docker container ls --all
 
 # verify "melbourne" container is accessible from browser
-Start-Process "http://localhost:9991/weather"
+Start-Process "http://localhost:9992/weather"
 
 # stop all containers
 docker container stop $(docker container ls -aq)
 
+# list containers
+docker container ls --all
+
 # remove all containers
 docker container rm $(docker container ls -aq)
 
+# list containers
+docker container ls --all
+
 # remove all images
 docker image rm -f $(docker image ls -aq)
+
+# list images
+docker image ls
